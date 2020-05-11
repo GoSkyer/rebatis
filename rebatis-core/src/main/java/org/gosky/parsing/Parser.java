b87d251a116a25778f390a10115c485d07ab4789
zoharSoul <hunji66@gmail.com>
2020-05-11 19:10:05 +0800

    优化参数解析代码

--- rebatis-core/src/main/java/org/gosky/parsing/Parser.java
+++ rebatis-core/src/main/java/org/gosky/parsing/Parser.java
@@ -27,78 +27,90 @@ public class Parser {
 
         if (parameters == null || parameters.length == 0) return new ParseSqlResult(sqlBeforeParse);
 
-        ArrayMap<String, Integer> positionMap = paramPositionMap(parameters);
-        List<ParameterRange> rangeList = paramInSqlPosition(sqlBeforeParse);
+//        ArrayMap<String, Integer> positionMap = paramPositionMap(parameters);
+//        List<ParameterRange> rangeList = paramInSqlPosition(sqlBeforeParse);
+        //解析方法参数
+        ParamNameResolver paramNameResolver = new ParamNameResolver(method);
+        Object parameterMappings = paramNameResolver.getNamedParams(args);
 
-        return replaceSqlWithParameters(sqlBeforeParse, rangeList, positionMap, args);
+        return replaceSqlWithParameters(sqlBeforeParse, parameterMappings);
 
     }
 
-    private ParseSqlResult replaceSqlWithParameters(String sqlBeforeParse, List<ParameterRange> rangeList, ArrayMap<String, Integer> positionMap, Object[] args) {
+    private ParseSqlResult replaceSqlWithParameters(String sqlBeforeParse, Object parameterMappings) {
+        List<String> rangeList = new ArrayList<>();
+        GenericTokenParser parser = new GenericTokenParser("#{", "}", new TokenHandler() {
+            @Override
+            public String handleToken(String content) {
+                rangeList.add(content);
+                return "?";
+            }
+        });
 
-        String sqlAfterParse = sqlBeforeParse;
+        String sqlAfterParse = parser.parse(sqlBeforeParse);
         List<Object> values = new LinkedList<>();
 
-        for (ParameterRange range : rangeList) {
-            String holder = "#{" + range.getValue() + "}";
-            sqlAfterParse = sqlAfterParse.replace(holder, "?");
-            values.add(args[positionMap.get(range.getValue())]);
+        for (String range : rangeList) {
+            if (parameterMappings instanceof Map) {
+                Object e = ((Map) parameterMappings).get(range);
+                values.add(e);
+            }
         }
 
         return new ParseSqlResult(sqlAfterParse, values);
 
     }
 
-    private ArrayMap<String, Integer> paramPositionMap(Parameter[] parameters) {
+//    private ArrayMap<String, Integer> paramPositionMap(Parameter[] parameters) {
+//
+//        ArrayMap<String, Integer> positionMap = new ArrayMap<>(parameters.length);
+//
+//        for (int i = 0; i < parameters.length; i++) {
+//            Parameter parameter = parameters[i];
+//            Param param = parameter.getAnnotation(Param.class);
+//
+//            if (param == null) continue;
+//
+//            positionMap.put(param.value(), i);
+//        }
+//
+//        return positionMap;
+//    }
 
-        ArrayMap<String, Integer> positionMap = new ArrayMap<>(parameters.length);
-
-        for (int i = 0; i < parameters.length; i++) {
-            Parameter parameter = parameters[i];
-            Param param = parameter.getAnnotation(Param.class);
-
-            if (param == null) continue;
-
-            positionMap.put(param.value(), i);
-        }
-
-        return positionMap;
-    }
-
-    private List<ParameterRange> paramInSqlPosition(String sqlBeforeParse) throws Exception {
-
-        List<ParameterRange> list = new LinkedList<>();
-
-        char[] sql = sqlBeforeParse.toCharArray();
-
-        int start = 0;
-        int end = 0;
-
-        while (start < sql.length) {
-
-            while (end < sql.length && sql[end] != '}') end++;
-            while (start < sql.length && sql[start] != '#') start++;
-
-            if (end == sql.length || start == sql.length) break;
-
-            if (sql[start] == '#' && sql[end] == '}') {
-
-                if (sql[start + 1] != '{') throw new Exception("SQL参数配置错误");
-
-                ParameterRange range = new ParameterRange();
-                range.setStart(start);
-                range.setEnd(end);
-                range.setValue(sqlBeforeParse.substring(start + 2, end));
-                list.add(range);
-
-                end++;
-                start = end;
-
-            }
-
-        }
-
-        return list;
-    }
+//    private List<ParameterRange> paramInSqlPosition(String sqlBeforeParse) throws Exception {
+//
+//        List<ParameterRange> list = new LinkedList<>();
+//
+//        char[] sql = sqlBeforeParse.toCharArray();
+//
+//        int start = 0;
+//        int end = 0;
+//
+//        while (start < sql.length) {
+//
+//            while (end < sql.length && sql[end] != '}') end++;
+//            while (start < sql.length && sql[start] != '#') start++;
+//
+//            if (end == sql.length || start == sql.length) break;
+//
+//            if (sql[start] == '#' && sql[end] == '}') {
+//
+//                if (sql[start + 1] != '{') throw new Exception("SQL参数配置错误");
+//
+//                ParameterRange range = new ParameterRange();
+//                range.setStart(start);
+//                range.setEnd(end);
+//                range.setValue(sqlBeforeParse.substring(start + 2, end));
+//                list.add(range);
+//
+//                end++;
+//                start = end;
+//
+//            }
+//
+//        }
+//
+//        return list;
+//    }
 
 }
